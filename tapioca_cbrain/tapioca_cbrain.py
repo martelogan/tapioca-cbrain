9e41432c3d8e849c3e4ce3ebbd4b1dccec033f4f
Logan Martel <logan.martel@outlook.com>
2019-04-01 19:23:08 -0400

    Implement skeleton cbrain api wrapper

--- tapioca_cbrain/tapioca_cbrain.py
+++ tapioca_cbrain/tapioca_cbrain.py
@@ -3,21 +3,22 @@
 from tapioca import (
     TapiocaAdapter, generate_wrapper_from_adapter, JSONAdapterMixin)
 
-
-from .resource_mapping import RESOURCE_MAPPING
+from resource_mapping import RESOURCE_MAPPING
 
 
 class CbrainClientAdapter(JSONAdapterMixin, TapiocaAdapter):
-    api_root = 'https://portal.cbrain.mcgill.ca'
+    api_root = 'https://portal.cbrain.mcgill.ca/'
     resource_mapping = RESOURCE_MAPPING
 
     def get_request_kwargs(self, api_params, *args, **kwargs):
-        params = super(CbrainClientAdapter, self).get_request_kwargs(
+        kwargs = super(CbrainClientAdapter, self).get_request_kwargs(
             api_params, *args, **kwargs)
 
-        
+        kwargs['headers'] = {'Accept': 'application/json'}
 
-        return params
+        kwargs['params'] = {'cbrain_api_token': api_params.get('cbrain_api_token')}
+
+        return kwargs
 
     def get_iterator_list(self, response_data):
         return response_data
